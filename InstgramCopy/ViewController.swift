a42b9f5d4a212fa02cb967f8c65cedc056e933d3
洪森達 <q793423@yahoo.com.tw>
2018-12-24 04:44:52 +0800

    Initial Commit

---
+++ InstgramCopy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstgramCopy
+//
+//  Created by 洪森達 on 2018/12/24.
+//  Copyright © 2018 洪森達. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
